55264dd6af7bae92b09bbcee2f7863ea213e6d61
Audrey Cooper <audrey_cooper@me.com>
2020-08-28 08:18:26 -0700

    iplemented 3 first promises in index 2

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
 const fetchMyIp = require('./iss_promised');
 const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTimes } = require('./iss_promised');
 
 fetchMyIP()
-  .then((body) => {
-    console.log(body)
-  })
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
--- iss_promised.js
+++ iss_promised.js
@@ -2,8 +2,17 @@ const request = require('request-promise-native');
 
 fetchMyIP = function () {
   return request('https://api.ipify.org?format=json')
-
 }
 
+const fetchCoordsByIP = function (body) {
+  const coords = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/json/${coords}`)
+}
 
-module.exports = { fetchMyIP };
+const fetchISSFlyOverTimes = function (body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  const url = request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+  return url;
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
